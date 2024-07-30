9730a72af02959d31ec76cb653f37046cf3d3438
daramdhanush <daramdhanush007@gmail.com>
2024-07-30 16:29:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,24 @@ import os
 from PIL import Image
 import numpy as np
 import pickle
-import tensorflow
+import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
-from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 from sklearn.neighbors import NearestNeighbors
 from numpy.linalg import norm
+import logging
 
-feature_list = np.array(pickle.load(open('embeddings.pkl','rb')))
-filenames = pickle.load(open('filenames.pkl','rb'))
+# Logging configuration
+logging.basicConfig(level=logging.DEBUG)
 
-model = ResNet50(weights='imagenet',include_top=False,input_shape=(224,224,3))
+feature_list = np.array(pickle.load(open('embeddings.pkl', 'rb')))
+filenames = pickle.load(open('filenames.pkl', 'rb'))
+
+model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
 model.trainable = False
 
-model = tensorflow.keras.Sequential([
+model = tf.keras.Sequential([
     model,
     GlobalMaxPooling2D()
 ])
@@ -25,13 +29,17 @@ st.title('Fashion Recommender System')
 
 def save_uploaded_file(uploaded_file):
     try:
-        with open(os.path.join('uploads',uploaded_file.name),'wb') as f:
+        if not os.path.exists('uploads'):
+            os.makedirs('uploads')
+        with open(os.path.join('uploads', uploaded_file.name), 'wb') as f:
             f.write(uploaded_file.getbuffer())
         return 1
-    except:
+    except Exception as e:
+        st.error(f"Error: {e}")
+        logging.error(f"Error: {e}")
         return 0
 
-def feature_extraction(img_path,model):
+def feature_extraction(img_path, model):
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
     expanded_img_array = np.expand_dims(img_array, axis=0)
@@ -41,7 +49,7 @@ def feature_extraction(img_path,model):
 
     return normalized_result
 
-def recommend(features,feature_list):
+def recommend(features, feature_list):
     neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
     neighbors.fit(feature_list)
 
@@ -49,30 +57,29 @@ def recommend(features,feature_list):
 
     return indices
 
-
 uploaded_file = st.file_uploader("Choose an image")
 if uploaded_file is not None:
     if save_uploaded_file(uploaded_file):
-        # display the file
+        # Display the file
         display_image = Image.open(uploaded_file)
-        st.image(display_image,width=300,caption="Your image")
-        # feature extract
-        features = feature_extraction(os.path.join("uploads",uploaded_file.name),model)
-        # recommendention
-        indices = recommend(features,feature_list)
+        st.image(display_image, width=300, caption="Your image")
+        # Feature extract
+        features = feature_extraction(os.path.join("uploads", uploaded_file.name), model)
+        # Recommendation
+        indices = recommend(features, feature_list)
 
-        # display
+        # Display
         st.write("Recommended images")
-        col1,col2,col3,col4,col5 = st.columns(5)
+        col1, col2, col3, col4, col5 = st.columns(5)
         with col1:
-            st.image(filenames[indices[0][0]],width=90)
+            st.image(filenames[indices[0][0]], width=90)
         with col2:
-            st.image(filenames[indices[0][1]],width=90)
+            st.image(filenames[indices[0][1]], width=90)
         with col3:
-            st.image(filenames[indices[0][2]],width=90)
+            st.image(filenames[indices[0][2]], width=90)
         with col4:
-            st.image(filenames[indices[0][3]],width=90)
+            st.image(filenames[indices[0][3]], width=90)
         with col5:
-            st.image(filenames[indices[0][4]],width=90)
+            st.image(filenames[indices[0][4]], width=90)
     else:
-        st.header("Some error occured in file upload")
+        st.header("Some error occurred in file upload")
